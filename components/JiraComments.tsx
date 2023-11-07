7e2539204c66d553938d24a82dcd3680fe4ee1f5
Anusheel Bhushan <anusheel@users.noreply.github.com>
2023-11-07 10:22:06 -0800

    Commit from Stub AI (#2)
    Co-authored-by: anusheel <ai@codestub.ai>

--- components/JiraComments.tsx
+++ components/JiraComments.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import JiraApi from 'jira-client';
 
 // Initialize Jira client
@@ -27,10 +27,14 @@ const JiraComments: React.FC<JiraCommentsProps> = ({ issueKey }) => {
   const [comments, setComments] = useState<Comment[]>([]);
   const [error, setError] = useState<Error | null>(null);
 
+  useEffect(() => {
+    fetchComments();
+  }, [issueKey]);
+
   const fetchComments = async () => {
     try {
       const issue = await jira.findIssue(issueKey);
-      setComments(issue.fields.comment.comments);
+      setComments(issue.fields.comment.comments || []);
     } catch (err) {
       setError(err as Error);
     }
@@ -49,12 +53,16 @@ const JiraComments: React.FC<JiraCommentsProps> = ({ issueKey }) => {
     <div className="p-4 rounded shadow bg-white">
       <h2 className="text-xl font-bold mb-2">Comments</h2>
       {error && <div className="text-red-500">{error.message}</div>}
-      {comments.map((comment, index) => (
-        <div key={index} className="border-b pb-2 mb-2">
-          <p className="text-sm">{comment.body}</p>
-          <p className="text-xs text-gray-500">{comment.author.displayName}</p>
-        </div>
-      ))}
+      {comments.length === 0 ? (
+        <p>No comments yet.</p>
+      ) : (
+        comments.map((comment, index) => (
+          <div key={index} className="border-b pb-2 mb-2">
+            <p className="text-sm">{comment.body}</p>
+            <p className="text-xs text-gray-500">{comment.author.displayName}</p>
+          </div>
+        ))
+      )}
       <button className="mr-2 py-1 px-3 rounded bg-blue-500 text-white" onClick={fetchComments}>Fetch Comments</button>
       <button className="py-1 px-3 rounded bg-green-500 text-white" onClick={addComment}>Add Comment</button>
     </div>
